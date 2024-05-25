45085a796f7b29415e3cb73ad1fed047c7e719e7
arodrigues <antonio.rodrigues@compta.pt>
2024-05-25 16:37:50 +0100

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "com.hackerrank.exercices"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
